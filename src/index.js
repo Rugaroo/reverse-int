cd701748d5c94ac021d71660b89f2ba4328260a6
Rugaroo <63457972+Rugaroo@users.noreply.github.com>
2020-10-18 13:27:51 +0300

    feat:add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse (str) {
+  if (str < 0) str *= -1;
+  str = String(str);
+
+  return Number(str.split('').reverse().join(''));
 }
